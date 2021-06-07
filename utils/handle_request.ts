923d8ea3c7c5db812a1b2dfdaad873ad3c886285
Tim Vick <t.vick@yahoo.com>
2021-06-07 05:26:40 +0000

    add preHandler function

--- utils/handle_request.ts
+++ utils/handle_request.ts
@@ -1,41 +1,15 @@
 import { Responses } from "./responses.ts";
 import { ServerRequest } from "https://deno.land/std@0.97.0/http/server.ts";
 import { ServerResponse } from "../types.ts";
-import { isPingRequest } from "./is_ping_request.ts";
-import { parseBody } from "./parse_body.ts";
-import { parseRequest } from "./parse_request.ts";
-import { validateRequest } from "./validate_request.ts";
+import { preHandler } from "./pre_handler.ts";
 
 export async function handleRequest(
   req: ServerRequest,
 ): Promise<ServerResponse> {
-  if (req.method !== "POST") {
-    return Responses.notFound();
-  }
-
-  const { signature, timestamp, rawBody, error: parseError } =
-    await parseRequest(
-      req,
-    );
-
-  if (parseError) {
-    return Responses.badRequest(parseError.message);
-  }
-
-  if (
-    !validateRequest(
-      signature as string,
-      timestamp as string,
-      rawBody as string,
-    )
-  ) {
-    return Responses.badRequest("Incorrect encoding");
-  }
-
-  const body = parseBody(rawBody as string);
-
-  if (isPingRequest(body)) {
-    return Responses.pong();
+  const payload = await preHandler(req);
+  
+  if (payload) {
+    return payload;
   }
 
   return Responses.ok("Got it.");
